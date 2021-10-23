c048df19e881b775037d8f86ef656a21557ff025
Ernesto Valecillos <evalecillos@gmail.com>
2021-10-22 21:15:48 -0500

    added express server + connection to db

--- server.js
+++ server.js
@@ -1,20 +1,29 @@
-const express = require('express');
+const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require("mysql2");
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.json({
-        message: "Hello World",
-    });
-});
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: "localhost",
+        // Your MySQL username,
+        user: "root",
+        // Your MySQL password
+        password: "30303030",
+        database: "election",
+    },
+    console.log("Connected to the election database.")
+);
+
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
-  res.status(404).end();
+    res.status(404).end();
 });
 
 app.listen(PORT, () => {
